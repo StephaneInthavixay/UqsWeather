ff2ec6e098717476ab6185b75f8c4457ef2b53e4
StayFan <s.inthavixay@gmail.com>
2024-06-22 14:41:37 +0200

    Add new Tests with NSubstitute mocks

--- Uqs.Weather.Tests.Unit/Fixtures/ClientStub.cs
+++ Uqs.Weather.Tests.Unit/Fixtures/ClientStub.cs
@@ -6,6 +6,7 @@ public class ClientStub : IClient
 {
     private readonly DateTime _now;
     private readonly IEnumerable<double> _sevenDaysTemps;
+    public Units? LastUnitSpy { get; set; }
 
     public ClientStub(DateTime now, IEnumerable<double> sevenDaysTemps)
     {
@@ -15,6 +16,7 @@ public class ClientStub : IClient
 
     public Task<OneCallResponse> OneCallAsync(decimal latitude, decimal longitude, IEnumerable<Excludes> excludes, Units unit)
     {
+        LastUnitSpy = unit;
         const int DAYS = 7;
         OneCallResponse res = new OneCallResponse();
         res.Daily = new Daily[DAYS];
@@ -24,7 +26,7 @@ public class ClientStub : IClient
             res.Daily[i] = new Daily();
             res.Daily[i].Dt = _now.AddDays(i);
             res.Daily[i].Temp = new Temp();
-            res.Daily[i].Temp.Day = -_sevenDaysTemps.ElementAt(i);
+            res.Daily[i].Temp.Day = _sevenDaysTemps.ElementAt(i);
         }
 
         return Task.FromResult(res);
--- Uqs.Weather.Tests.Unit/WeatherForecastControllerTests.cs
+++ Uqs.Weather.Tests.Unit/WeatherForecastControllerTests.cs
@@ -1,4 +1,6 @@
+using AdamTibi.OpenWeather;
 using Microsoft.Extensions.Logging.Abstractions;
+using NSubstitute;
 using Uqs.Weather.Controllers;
 using Uqs.Weather.Tests.Unit.Fixtures;
 
@@ -31,7 +33,7 @@ public class WeatherForecastControllerTests
         const double nextDayTemp = 3.3;
         const double day5Temp = 7.7;
         var today = new DateTime(2022, 1, 1);
-        var realWeatherTemps = new double[]
+        var realWeatherTemps = new[]
         {
             2, nextDayTemp, 4, 5.5, 6, day5Temp, 8
         };
@@ -42,6 +44,99 @@ public class WeatherForecastControllerTests
         IEnumerable<WeatherForecast> wfs = await controller.GetReal();
 
         // Assert
-        Assert.Equal(-3, wfs.First().TemperatureC);
+        Assert.Equal(3, wfs.First().TemperatureC);
+    }
+
+    [Fact]
+    public async Task GetReal_RequestsToOpenWeather_MetricUnitIsUsed()
+    {
+        // Arrange
+        var realWeatherTemps = new double[]
+        {
+            1, 2, 3, 4, 5, 6, 7
+        };
+        var clientStub = new ClientStub(default(DateTime), realWeatherTemps);
+        var controller = new WeatherForecastController(null!, clientStub, null!, null!);
+        
+        // Act
+        var _ = await controller.GetReal();
+        
+        // Assert
+        Assert.NotNull(clientStub.LastUnitSpy);
+        Assert.Equal(Units.Metric, clientStub.LastUnitSpy!.Value);
+    }
+    
+    [Fact]
+    public async Task GetReal_NotInterestedInTodayWeather_WFStartsFromNextDayWithNSubstitute()
+    {
+        // Arrange
+        const double nextDayTemp = 3.3;
+        const double day5Temp = 7.7;
+        var today = new DateTime(2022, 1, 1);
+        var realWeatherTemps = new[]
+        {
+            2, nextDayTemp, 4, 5.5, 6, day5Temp, 8
+        };
+        
+        var clientMock = Substitute.For<IClient>();
+        clientMock.OneCallAsync(Arg.Any<decimal>(), Arg.Any<decimal>(), Arg.Any<IEnumerable<Excludes>>(),
+            Arg.Any<Units>())
+            .Returns(x =>
+            {
+                const int DAYS = 7;
+                OneCallResponse res = new OneCallResponse();
+                res.Daily = new Daily[DAYS];
+                for (int i = 0; i < DAYS; i++)
+                {
+                    res.Daily[i] = new Daily();
+                    res.Daily[i].Dt = today.AddDays(i);
+                    res.Daily[i].Temp = new Temp();
+                    res.Daily[i].Temp.Day = realWeatherTemps.ElementAt(i);
+                }
+                return Task.FromResult(res);
+            });
+        var controller = new WeatherForecastController(null!, clientMock, null!, null!);
+
+        // Act
+        IEnumerable<WeatherForecast> wfs = await controller.GetReal();
+
+        // Assert
+        Assert.Equal(3, wfs.First().TemperatureC);
+    }
+    
+    [Fact]
+    public async Task GetReal_RequestsToOpenWeather_MetricUnitIsUsedWithNSubtitute()
+    {
+        // Arrange
+        var realWeatherTemps = new double[]
+        {
+            1, 2, 3, 4, 5, 6, 7
+        };
+        var clientMock = Substitute.For<IClient>();
+        clientMock.OneCallAsync(Arg.Any<decimal>(), Arg.Any<decimal>(), Arg.Any<IEnumerable<Excludes>>(),
+                Arg.Any<Units>())
+            .Returns(x =>
+            {
+                const int DAYS = 7;
+                OneCallResponse res = new OneCallResponse();
+                res.Daily = new Daily[DAYS];
+                for (int i = 0; i < DAYS; i++)
+                {
+                    res.Daily[i] = new Daily();
+                    res.Daily[i].Dt = default(DateTime).AddDays(i);
+                    res.Daily[i].Temp = new Temp();
+                    res.Daily[i].Temp.Day = realWeatherTemps.ElementAt(i);
+                }
+                return Task.FromResult(res);
+            });
+        var controller = new WeatherForecastController(null!, clientMock, null!, null!);
+        
+        // Act
+        var _ = await controller.GetReal();
+        
+        // Assert
+        await clientMock.Received().OneCallAsync(
+            Arg.Any<decimal>(), Arg.Any<decimal>(), Arg.Any<IEnumerable<Excludes>>(),
+            Arg.Is<Units>(x => x == Units.Metric));
     }
 }
